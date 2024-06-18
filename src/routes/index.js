b3a6eb1437a9e6d61eefc845a1a6e63b5bf31e2d
Najwan <najwanabiy@gmail.com>
2024-06-18 19:48:36 +0700

    Penambahan API

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const verifyToken = require('../middleware');
-const firebaseAuthController = require('../controllers/controller.js');
-const { storeData } = require('../services/storeData.js');
+const { getSequentialAudio } = require('../controllers/audioUp');
+const firebaseAuthController = require('../controllers/auth');
+const storeKids = require ('../controllers/storeKids')
 // Auth routes
 router.post('/register', firebaseAuthController.registerUser);
 router.post('/login', firebaseAuthController.loginUser);
-router.post('/api/logout', firebaseAuthController.logoutUser);
+router.post('/logout', firebaseAuthController.logoutUser);
 router.post('/reset-password', firebaseAuthController.resetPassword);
 
-// Store Kids
-router.post('/store-data', firebaseAuthController.storeData);
+// Store Kids data
+router.post('/storeKids', storeKids.storeData);
+
+router.get('/sequential-audio', getSequentialAudio);
 
 
 module.exports = router;
